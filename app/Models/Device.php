6555f06252d093fb1b322caa0c3cfd9cb25e6ba9
darwin ibay <darwin.i@shoredigitalinc.com>
2025-01-09 22:19:24 +0800

    uipdates

--- app/Models/Device.php
+++ app/Models/Device.php
@@ -29,17 +29,17 @@ class Device extends Model implements HasMedia
         return $this->hasOne(Vehicle::class, 'device_id', 'id: ');
     }
 
-    public function location(): Attribute
-    {
-        return Attribute::make(
-            get: fn($value, $attributes) => json_encode([
-                'lat' => (float) $attributes['latitude'],
-                'lng' => (float) $attributes['longitude'],
-            ]),
-            set: fn($value) => [
-                'latitude' => $value['lat'],
-                'longitude' => $value['lng'],
-            ],
-        );
-    }
+    // public function location(): Attribute
+    // {
+    //     return Attribute::make(
+    //         get: fn($value, $attributes) => json_encode([
+    //             'lat' => (float) $attributes['latitude'],
+    //             'lng' => (float) $attributes['longitude'],
+    //         ]),
+    //         set: fn($value) => [
+    //             'latitude' => $value['lat'],
+    //             'longitude' => $value['lng'],
+    //         ],
+    //     );
+    // }
 }
